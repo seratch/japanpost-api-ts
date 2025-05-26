bee5b08ddaa82d22a44a5c44a54cb8d75a9cf8cd
Kazuhiro Sera <seratch@gmail.com>
2025-05-26 22:24:03 +0900

    Fix a bug

--- src/Logger.ts
+++ src/Logger.ts
@@ -3,7 +3,7 @@ import debug from "debug";
 const logger = debug("japanpost-api");
 
 export function debugLog(message: () => string) {
-  if (logger.enabled) {
+  if (debug.enabled(logger.namespace)) {
     const msg = message();
     if (msg) {
       logger(msg);
